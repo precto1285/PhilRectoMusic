27203583e1f67873bf6f70f28673bbb30ed88b99
Philippe Recto <precto1285@gmal.com>
2018-03-18 10:31:33 -0700

    added firebase link

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDI90cbRL6b3ChjxbYFfd7Gq3gVqV74IvM",
+    authDomain: "philmusic-63c12.firebaseapp.com",
+    databaseURL: "https://philmusic-63c12.firebaseio.com",
+    projectId: "philmusic-63c12",
+    storageBucket: "",
+    messagingSenderId: "694275992182"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
