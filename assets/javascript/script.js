f1180dd76945488b6174142ac96f92c529bab594
Philippe Recto <precto1285@gmal.com>
2018-03-18 14:02:01 -0700

    buttons test

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 // Initialize Firebase
-var config = {
+/* var config = {
   apiKey: "AIzaSyDI90cbRL6b3ChjxbYFfd7Gq3gVqV74IvM",
   authDomain: "philmusic-63c12.firebaseapp.com",
   databaseURL: "https://philmusic-63c12.firebaseio.com",
@@ -20,37 +20,38 @@ var storageRef = storage.ref();
 var imagesRef = storageRef.child('images');
 
 var mp3Ref = storageRef.child('mp3s');
-
+*/
 
 //main info buttons
-$('#what').on("click", function(){
+$('#what').on("click", function () {
   console.log("yesA");
-  $('#where').on("click", function(){
-    console.log("yesB");
-
+});
+$('#where').on("click", function () {
+  console.log("yesB");
+});
 //sidebar music buttons
-$('#bHead').on("click", function(){
+$('#bHead').on("click", function () {
   console.log("yes0");
 });
-$('#tVah').on("click", function(){
+$('#tVah').on("click", function () {
   console.log("yes1");
 });
-$('#scTmachine').on("click", function(){
+$('#scTmachine').on("click", function () {
   console.log("yes2");
 });
-$('#aSessions').on("click", function(){
+$('#aSessions').on("click", function () {
   console.log("yes3");
 });
-$('#sPhome').on("click", function(){
+$('#sPhome').on("click", function () {
   console.log("yes4");
 });
-$('#pheel').on("click", function(){
+$('#pheel').on("click", function () {
   console.log("yes5");
 });
-$('#ymr').on("click", function(){
+$('#ymr').on("click", function () {
   console.log("yes6");
 });
-$('#pHome').on("click", function(){
+$('#pHome').on("click", function () {
   console.log("yes7");
 });
 
