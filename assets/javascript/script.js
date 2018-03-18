a17baa1b1610da0b1f2d917dfc29e86447705680
Philippe Recto <precto1285@gmal.com>
2018-03-18 13:47:31 -0700

    web refresh

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,27 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDI90cbRL6b3ChjxbYFfd7Gq3gVqV74IvM",
-    authDomain: "philmusic-63c12.firebaseapp.com",
-    databaseURL: "https://philmusic-63c12.firebaseio.com",
-    projectId: "philmusic-63c12",
-    storageBucket: "",
-    messagingSenderId: "694275992182"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyDI90cbRL6b3ChjxbYFfd7Gq3gVqV74IvM",
+  authDomain: "philmusic-63c12.firebaseapp.com",
+  databaseURL: "https://philmusic-63c12.firebaseio.com",
+  projectId: "philmusic-63c12",
+  storageBucket: "",
+  messagingSenderId: "694275992182"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+//Add to database
+var database = firebase.database();
+
+//reference storage
+var storage = firebase.storage();
+
+var storageRef = storage.ref();
+
+var imagesRef = storageRef.child('images');
+
+var mp3Ref = storageRef.child('mp3s');
+
+
+
+
 
