62a8d1e34c0bd33e3598c692d550bf99af44e4af
Philippe Recto <precto1285@gmal.com>
2018-03-18 13:57:31 -0700

    added button functions in js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -22,6 +22,39 @@ var imagesRef = storageRef.child('images');
 var mp3Ref = storageRef.child('mp3s');
 
 
+//main info buttons
+$('#what').on("click", function(){
+  console.log("yesA");
+  $('#where').on("click", function(){
+    console.log("yesB");
+
+//sidebar music buttons
+$('#bHead').on("click", function(){
+  console.log("yes0");
+});
+$('#tVah').on("click", function(){
+  console.log("yes1");
+});
+$('#scTmachine').on("click", function(){
+  console.log("yes2");
+});
+$('#aSessions').on("click", function(){
+  console.log("yes3");
+});
+$('#sPhome').on("click", function(){
+  console.log("yes4");
+});
+$('#pheel').on("click", function(){
+  console.log("yes5");
+});
+$('#ymr').on("click", function(){
+  console.log("yes6");
+});
+$('#pHome').on("click", function(){
+  console.log("yes7");
+});
+
+
 
 
 
